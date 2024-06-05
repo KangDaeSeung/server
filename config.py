594d497693c5f6d10fdb99610c8a5f001c2fd8bc
KangDaeSeung <kang11128@gmail.com>
2024-06-05 09:32:21 +0900

    three

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ SECRET_KEY ="dev" #os.urandom(24)
 #SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(BASE_DIR,'pybo.db'))
 SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://root:1234@localhost/study_flask'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+SECRET_KEY="dev"
