db8b5552fe1a15d46d1b86701e95bf4d79a4393a
KangDaeSeung <kang11128@gmail.com>
2024-06-10 11:58:59 +0900

    test

--- study_flask/views/main_views.py
+++ study_flask/views/main_views.py
@@ -1,13 +1,36 @@
 from flask import Blueprint, url_for
 from werkzeug.utils import redirect
-from ..models.models import Question
 
-bp=Blueprint('main',__name__,url_prefix='/')
-@bp.route('/hello')
-def hello():
-    return 'Wellcome! \
-        It\'s a Flask!!!'
+import pymysql.cursors
+
+
+from datetime import datetime
+
+bp = Blueprint('main',__name__,url_prefix='/')
+
+
+@bp.route('/test')
+def test():
+    return '어서와 플라스크는 처음이지!'
 
 @bp.route('/')
 def index():
     return redirect(url_for('question._list'))
+
+
+
+@bp.route('/test1/')
+def test1():
+    connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='1234',
+                             database='study_flask',
+                             cursorclass=pymysql.cursors.DictCursor)
+    sql = "INSERT INTO question (subject, content, create_date) VALUES (%s, %s, %s)"
+    cursor = connection.cursor()
+    cursor.execute(sql, ('테스트1', '테스트용 질문', datetime.now()))
+    connection.commit()
+    connection.close()
+    return 'OK'
+
+    
